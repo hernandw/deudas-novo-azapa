b31d231afd91c6735ff0e546f71698ec0cbe4915
Williams Hernandez <hernandw@gmail.com>
2022-12-16 16:59:09 -0300

    mensaje de no existe

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,11 +9,10 @@ const formatter = new Intl.NumberFormat("en-ES", {
   minimumFractionDigits: 0,
 });
 
-
 function upperCase() {
-    var x=document.getElementById("apto").value
-    document.getElementById("apto").value=x.toUpperCase()
- }
+  var x = document.getElementById("apto").value;
+  document.getElementById("apto").value = x.toUpperCase();
+}
 
 function getDatos() {
   const xhttp = new XMLHttpRequest();
@@ -29,42 +28,45 @@ function getDatos() {
       resultado.innerHTML = "";
 
       for (let item of datos)
-        if ((consulta == item.apto) &&(item.deuda <= 0) ) {
+        if (consulta == item.apto && item.deuda <= 0) {
           resultado.innerHTML = `
-        <p class="fw-bold">Apartamento: <span class="fw-normal">${
-          item.apto
-        }</span></p>
-        <p class="fw-bold">Deuda Mes: <span class="fw-normal">${formatter.format(
-          item.mes_anterior
-        )}</span></p>
-        <p class="fw-bold">Deuda Atrasada: <span class="fw-normal">${formatter.format(
-          item.deuda
-        )}</span></p>
-        <p class="fw-bold">Total Deuda: <span class="fw-normal">${formatter.format(
-          item.total_deuda
-        )}</span></p>
-        <p class="fw-bold text-success">Estas Habilitado para votar</p>
-           
-            `;
-        }
-        else if((consulta == item.apto) &&(item.deuda >= 0) ){
-            resultado.innerHTML = `
-        <p class="fw-bold">Apartamento: <span class="fw-normal">${
-          item.apto
-        }</span></p>
-        <p class="fw-bold">Deuda Mes: <span class="fw-normal">${formatter.format(
-          item.mes_anterior
-        )}</span></p>
-        <p class="fw-bold">Deuda Atrasada: <span class="fw-normal">${formatter.format(
-          item.deuda
-        )}</span></p>
-        <p class="fw-bold">Total Deuda: <span class="fw-normal">${formatter.format(
-          item.total_deuda
-        )}</span></p>
-        <p class="text-danger fw-bold"> No Estas Habilitado para votar</p>
-        
-           
-            `;
+  <p class="fw-bold">Apartamento: <span class="fw-normal">${
+    item.apto
+  }</span></p>
+  <p class="fw-bold">Deuda Mes: <span class="fw-normal">${formatter.format(
+    item.mes_anterior
+  )}</span></p>
+  <p class="fw-bold">Deuda Atrasada: <span class="fw-normal">${formatter.format(
+    item.deuda
+  )}</span></p>
+  <p class="fw-bold">Total Deuda: <span class="fw-normal">${formatter.format(
+    item.total_deuda
+  )}</span></p>
+  <p class="fw-bold text-success">Estas Habilitado para votar</p>
+     
+      `;
+          return;
+        } else if (consulta == item.apto && item.deuda >= 0) {
+          resultado.innerHTML = `
+  <p class="fw-bold">Apartamento: <span class="fw-normal">${
+    item.apto
+  }</span></p>
+  <p class="fw-bold">Deuda Mes: <span class="fw-normal">${formatter.format(
+    item.mes_anterior
+  )}</span></p> 
+  <p class="fw-bold">Deuda Atrasada: <span class="fw-normal">${formatter.format(
+    item.deuda
+  )}</span></p>
+  <p class="fw-bold">Total Deuda: <span class="fw-normal">${formatter.format(
+    item.total_deuda
+  )}</span></p>
+  <p class="text-danger fw-bold"> No Estas Habilitado para votar</p>
+  
+    
+      `;
+          return;
+        } else {
+          resultado.innerHTML = `<p class="text-danger fw-bold">El apartamento ${consulta} no existe</p>`;
         }
     }
   };
